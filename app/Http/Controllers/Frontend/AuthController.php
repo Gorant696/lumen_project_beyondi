f24b84f8a2a4306a27145f4187a4c58e1a52eb83
gorant696 <axelfoley@net.hr>
2017-11-09 14:46:14 +0100

    Merge branch 'develop' of https://github.com/algebrateam/phpdev2017 into develop
    # Conflicts:
    #	gtolusic/Index.php

--- app/Http/Controllers/Frontend/AuthController.php
+++ app/Http/Controllers/Frontend/AuthController.php
@@ -3,17 +3,14 @@
 namespace App\Http\Controllers\Frontend;
 
 use Illuminate\Http\Request;
-use App\User;
-use JWTAuth;
 use App\Http\Controllers\Controller;
 
 class AuthController extends Curlhelper {
 
-
     public function loginUser(Request $request) {
 
         $curl = curl_init();
-        
+
         curl_setopt_array($curl, array(
             CURLOPT_URL => 'http://beyondi.loc/login',
             CURLOPT_POST => 1,
@@ -36,75 +33,68 @@ class AuthController extends Curlhelper {
 
         //neki return s porukom, iskemijat
     }
-    
-    
-    public function registeruser(Request $request){
-        
+
+    public function registeruser(Request $request) {
+
         $data = array(
-                'email' => $request->input('email'),
-                'password' => $request->input('password'),
-                'name'=>$request->input('name'),
-                'role'=>$request->input('role')
-            );
-        
-         $decoded = $this->post_curl('users', $data);
-            dd($decoded);
-        
-         //neki return s porukom, iskemijat
-        
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+            'name' => $request->input('name'),
+            'role' => $request->input('role')
+        );
+
+        $decoded = $this->post_curl('users', $data);
+        dd($decoded);
+
+        //neki return s porukom, iskemijat
     }
 
-    
-    public function deleteuser($id){
-        
-          $decoded = $this->delete_curl('users', $id);
-            dd($decoded);
-        
-           //neki return s porukom, iskemijat
+    public function deleteuser($id) {
+
+        $decoded = $this->delete_curl('users', $id);
+        dd($decoded);
+
+        //neki return s porukom, iskemijat
     }
-    
+
     public function edit($id, Request $request) {
-        
-        $data=array(
-                'email' => $request->input('email'),
-                'password' => $request->input('password'),
-                'name' => $request->input('name')
-            );
-        
-           $decoded = $this->put_curl('users', $id, $data);
-            dd($decoded);
-       
+
+        $data = array(
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+            'name' => $request->input('name')
+        );
+
+        $decoded = $this->put_curl('users', $id, $data);
+        dd($decoded);
+
 
         //neki return s porukom, iskemijat
-        
     }
-    
-    public function addroletouser($id, Request $request){
-        
-         $data=array(
-                'role' => $request->input('role'),
-            );
-        
-           $decoded = $this->put_curl('addrole', $id, $data);
-            dd($decoded);
+
+    public function addroletouser($id, Request $request) {
+
+        $data = array(
+            'role' => $request->input('role'),
+        );
+
+        $decoded = $this->put_curl('addrole', $id, $data);
+        dd($decoded);
 
         //neki return s porukom, iskemijat
-
     }
-    
-       public function removerolefromuser($id, Request $request){
-           
-             $data=array(
-                'role' => $request->input('role'),
-            );
-           
-            $decoded = $this->put_curl('removerole', $id, $data);
-            dd($decoded);
-        
+
+    public function removerolefromuser($id, Request $request) {
+
+        $data = array(
+            'role' => $request->input('role'),
+        );
+
+        $decoded = $this->put_curl('removerole', $id, $data);
+        dd($decoded);
+
         //neki return s porukom, iskemijat
-
     }
-    
 
     public function findme() {
 
@@ -113,35 +103,29 @@ class AuthController extends Curlhelper {
 
         return view('frontend.findme', compact('decoded'));
     }
-    
-    
-    public function findone($id){
-        
-         $decoded = $this->get_curl("users/$id");
-         dd($decoded);
+
+    public function findone($id) {
+
+        $decoded = $this->get_curl("users/$id");
+        dd($decoded);
 
         //retur view (compact decoded), ne zaboraviti makniti dd iznad ovog teksta
-        
     }
-    
-     public function findall() {
-         
-          $decoded = $this->get_curl('users');
+
+    public function findall() {
+
+        $decoded = $this->get_curl('users');
         dd($decoded);
 
         //retur view (compact decoded), ne zaboraviti makniti dd iznad ovog teksta
-         
-     }
-     
-      public function logoutuser() {
-         
-     $decoded = $this->get_curl('logout');
+    }
+
+    public function logoutuser() {
+
+        $decoded = $this->get_curl('logout');
         dd($decoded);
 
         //retur view (compact decoded), ne zaboraviti makniti dd iznad ovog teksta
-         
-     }
-     
-   
+    }
 
 }
--- app/Http/Controllers/Frontend/Curlhelper.php
+++ app/Http/Controllers/Frontend/Curlhelper.php
@@ -3,56 +3,24 @@
 namespace App\Http\Controllers\Frontend;
 
 use Illuminate\Http\Request;
-use App\User;
-use JWTAuth;
 use App\Http\Controllers\Controller;
 
-class Curlhelper extends Controller  {
-    
-        
-    protected $baseurl='http://beyondi.loc/';
-    
-    private function curl(){
-        
-        $file = storage_path("tokens/token.txt");
-        $content = file_get_contents($file);
-        
-        
-        
-        $curl = curl_init();
-        
-         curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-            "Authorization: Bearer $content",
-        ));
-         
-         
-          $decoded = json_decode(curl_exec($curl));
+class Curlhelper extends Controller {
 
-        curl_close($curl);
-
-        return $decoded;
-
-        
-    }
+    protected $baseurl = 'http://beyondi.loc/';
     
 
- 
-    protected function get_curl($url) {
+    protected function curlfunc($data) {
 
         $file = storage_path("tokens/token.txt");
         $content = file_get_contents($file);
-
         $curl = curl_init();
-        
-         curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
             "Authorization: Bearer $content",
         ));
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "$this->baseurl$url",
-            CURLOPT_RETURNTRANSFER => 1,
-        ));
-       
+        curl_setopt_array($curl, $data);
 
         $decoded = json_decode(curl_exec($curl));
 
@@ -60,82 +28,50 @@ class Curlhelper extends Controller  {
 
         return $decoded;
     }
-    
-    protected function post_curl($url, $data){
-        
-             $file = storage_path("tokens/token.txt");
-            $content = file_get_contents($file);
-        
-            $curl = curl_init();
-            
-             curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-            "Authorization: Bearer $content",
-        ));
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL =>"$this->baseurl$url",
+    protected function get_curl($url) {
+
+        $data = array(
+            CURLOPT_URL => "$this->baseurl$url",
+            CURLOPT_RETURNTRANSFER => 1,
+        );
+
+        return $this->curlfunc($data);
+    }
+
+    protected function post_curl($url, $data) {
+
+        $data = array(
+            CURLOPT_URL => "$this->baseurl$url",
             CURLOPT_POST => 1,
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_POSTFIELDS => $data,
-        ));
+        );
 
-        $decoded = json_decode(curl_exec($curl));
-        curl_close($curl);
-
-        return $decoded;
-        
+        return $this->curlfunc($data);
     }
 
     protected function put_curl($url, $id, $data) {
 
 
-        $file = storage_path("tokens/token.txt");
-        $content = file_get_contents($file);
-
-
-        $curl = curl_init();
-
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-            "Authorization: Bearer $content",
-        ));
-
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
-
-        curl_setopt_array($curl, array(
+        $data = array(
             CURLOPT_URL => "$this->baseurl$url/$id",
             CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_CUSTOMREQUEST => 'PUT',
             CURLOPT_POSTFIELDS => http_build_query($data)
-        ));
+        );
 
-        $decoded = json_decode(curl_exec($curl));
-        curl_close($curl);
-
-        return $decoded;
+        return $this->curlfunc($data);
     }
 
     protected function delete_curl($url, $id) {
 
-        $file = storage_path("tokens/token.txt");
-        $content = file_get_contents($file);
-
-
-        $curl = curl_init();
-
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-            "Authorization: Bearer $content",
-        ));
-
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
-
-        curl_setopt_array($curl, array(
+       $data= array(
             CURLOPT_URL => "$this->baseurl$url/$id",
             CURLOPT_RETURNTRANSFER => 1,
-        ));
+        );
 
-        $decoded = json_decode(curl_exec($curl));
-        curl_close($curl);
-
-        return $decoded;
+      return $this->curlfunc($data);
     }
 
 }
